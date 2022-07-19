bdca59e7da172caa406a54a9af0fd934192db97b
Alexandru Chirea <alexandru-cornel.chirea@johnlewis.co.uk>
2022-07-19 10:46:42 +0300

    Start

--- src/main/java/com/example/springwebtraining/controller/ProductsController.java
+++ src/main/java/com/example/springwebtraining/controller/ProductsController.java
@@ -1,12 +1,7 @@
 package com.example.springwebtraining.controller;
 
-import com.example.springwebtraining.model.PriceRange;
-import com.example.springwebtraining.model.Product;
 import com.example.springwebtraining.service.ProductService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -18,25 +13,12 @@ public class ProductsController {
         this.productService = productService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAll() {
-        return ResponseEntity.ok(productService.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getById(@PathVariable("id") String id) throws Exception {
-        return ResponseEntity.ok(productService.findById(id));
-    }
-
-    @GetMapping("/filter")
-    public ResponseEntity<List<Product>> getByPrice(@RequestParam("minPrice") Double minPrice,
-                                                    @RequestParam("maxPrice") Double maxPrice) {
-        return ResponseEntity.ok(productService.findAllByPrice(new PriceRange(minPrice, maxPrice)));
-    }
-
-    @PostMapping
-    public ResponseEntity<Product> create(@RequestBody Product product) {
-        return ResponseEntity.ok(productService.create(product));
-    }
+    /**
+     * TODO
+     *  1. GET all products
+     *  2. GET a product by id
+     *  3. GET products based on price (min/max)
+     *  4. CREATE a product
+     */
 
 }
